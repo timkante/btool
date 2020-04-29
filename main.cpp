e4fa821236bca19faa3f24ec38a55fd6e0ea808e
Tim Kante <tim.kante@staffbase.com>
2020-04-29 20:08:34 +0200

    :construction_worker: testing TravisCI

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     std::cout << "Hello, World!\n2 + 2 is "
         << add(2, 2)
-        << "\nAnd this is a Travis Test!"
+        << "\nAnd this is another Travis Test!"
         << std::endl;
     return 0;
 }
