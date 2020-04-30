b4ea6b514fcc3ceede395acec200b490aa97008b
Tim Kante <tim.kante@staffbase.com>
2020-04-30 17:39:49 +0200

    :bug: introducing a bug to test SonarQube

--- src/add.cpp
+++ src/add.cpp
@@ -5,5 +5,7 @@
 #include <add.hpp>
 
 int add(int x, int y){
+    int abc(17);
+    abc = 27;
     return x + y;
 }
--- test/addTest.cpp
+++ test/addTest.cpp
@@ -6,7 +6,9 @@
 #include <add.hpp>
 
 TEST(addTest, addFunctionTest){
+    auto* test = new(int);
     ASSERT_EQ(add(3, 4), 7);
+    test = nullptr;
     ASSERT_EQ(add(0, 0), 0);
     ASSERT_EQ(add(12, 26), 38);
 }
