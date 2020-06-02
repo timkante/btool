734027f29e7a5610011025a63b09f08a93835a27
Tim Kante <tim.kante@staffbase.com>
2020-06-02 11:23:52 +0200

    :fire: removed unused import

--- src/TranslationTable.cpp
+++ src/TranslationTable.cpp
@@ -4,7 +4,6 @@
 
 #include "TranslationTable.hpp"
 #include <boost/property_tree/json_parser.hpp>
-#include <iostream>
 #include <sstream>
 
 TranslationTable::TranslationTable(std::stringstream file) {
