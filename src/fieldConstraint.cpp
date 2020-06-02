0522e19b2ae1214f8315f0711fe9a9d4479bff62
SaskiaHeinrich <saskia@bsphe.de>
2020-06-02 11:04:20 +0200

    Erstellung der Klassen fieldConstraint und StyleProperties

---
+++ src/fieldConstraint.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Saskia on 02.06.2020.
+//
+
+#include "fieldConstraint.h"
+#include <regex>
+
+using namespace std;
+
+fieldConstraint::fieldConstraint(String fieldName, String fieldFormat) {
+    name = fieldName;
+    format = fieldFormat; // Datentyp zu regex ändern?
+};
+
+
+bool matchesRegex(String dataFormat, String fieldFormat) {
+    if (regex_match(dataFormat,fieldFormat))
+        return true;
+    else
+        return false;
+}
+
+bool matchesConstraint(String value, Field field) {
+    if (field.name == name) {
+        if (matchesRegex(value, format))
+            return true;
+        else
+            return false;
+    };
+}
+
+
+
---
+++ src/styleProperties.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Saskia on 02.06.2020.
+//
+
+#include "styleProperties.h"
