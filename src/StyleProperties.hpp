21c27a2f97e3e6ae6e77dccf173159e46c945b4e
Rasheed Break <rashidkatamesh@Rashids-Mac.local>
2020-06-03 20:54:14 +0200

    :bug: fixed compiled errors

---
+++ src/StyleProperties.hpp
@@ -0,0 +1,17 @@
+#include "FieldConstraint.hpp"
+#include <vector>
+
+#ifndef BIBPARSER_STYLEPROPERTIES_HPP
+#define BIBPARSER_STYLEPROPERTIES_HPP
+
+#include <string>
+#include <FieldConstraint.hpp>
+
+struct StyleProperties {
+    std::string name;
+    std::vector<FieldConstraint> requiredFields;
+    std::vector<FieldConstraint> optionalFields;
+};
+
+
+#endif
