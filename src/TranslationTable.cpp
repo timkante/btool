b53111a44736906ac01b1294e15400548f03a960
Tim Kante <tim.kante@staffbase.com>
2020-06-17 13:40:54 +0200

    :bug::recycle: simplified and fixed boost-usage to support v1.61

--- src/TranslationTable.cpp
+++ src/TranslationTable.cpp
@@ -28,35 +28,29 @@ auto TranslationTable::parseCommonProperties() const -> StyleProperties {
                            [](const boost::property_tree::ptree::value_type &field) {
                                return field.first.empty();
                            }));
-        std::vector<std::optional<FieldConstraint>> resultCollector;
-        std::transform(
-                std::cbegin(node),
-                std::cend(node),
-                std::back_inserter(resultCollector),
-                [](const boost::property_tree::ptree::value_type &field) -> std::optional<FieldConstraint> {
-                    const auto title = field.second.get_child_optional("title");
-                    const auto format = field.second.get_child_optional("format");
-                    if (title.has_value() && format.has_value()) {
-                        return FieldConstraint(title.value().data(), std::regex{format.value().data()});
-                    } else {
-                        return std::nullopt;
-                    }
-                });
-
-        std::vector<FieldConstraint> validResults;
-        for (auto element : resultCollector){
-            if (element.has_value() && !element.value().name.empty())
-                validResults.push_back(element.value());
-        }
-        return validResults;
+        std::vector<FieldConstraint> fields;
+        std::for_each(std::cbegin(node),
+                      std::cend(node),
+                      [&fields](const boost::property_tree::ptree::value_type &field) {
+                          const boost::optional<const boost::property_tree::ptree &> title =
+                                  field.second.get_child_optional("title");
+                          const boost::optional<const boost::property_tree::ptree &> format =
+                                  field.second.get_child_optional("format");
+                          if (title && format && !title.value().data().empty() && !format.value().data().empty()) {
+                              fields.emplace_back(title.value().data(), std::regex{format.value().data()});
+                          }
+                      });
+        return fields;
     };
 
-    const auto requiredFieldsNode = contents.get_child_optional("commonRequiredFields");
-    const auto optionalFieldsNode = contents.get_child_optional("commonOptionalFields");
+    const boost::optional<const boost::property_tree::ptree &> requiredFieldsNode =
+            contents.get_child_optional("commonRequiredFields");
+    const boost::optional<const boost::property_tree::ptree &> optionalFieldsNode =
+            contents.get_child_optional("commonOptionalFields");
 
     return StyleProperties("_common",
-                           requiredFieldsNode.has_value() ? parseConstraintNode(requiredFieldsNode.value())
-                                                          : std::vector<FieldConstraint>{},
-                           optionalFieldsNode.has_value() ? parseConstraintNode(optionalFieldsNode.value())
-                                                          : std::vector<FieldConstraint>{});
+                           requiredFieldsNode ? parseConstraintNode(requiredFieldsNode.value())
+                                              : std::vector<FieldConstraint>{},
+                           optionalFieldsNode ? parseConstraintNode(optionalFieldsNode.value())
+                                              : std::vector<FieldConstraint>{});
 }
