6406852e507c861bf907e63a936ec2d513568da2
Tim Kante <tim@timkante.dev>
2020-07-06 15:56:22 +0200

    :pencil::sparkles: added some Doxygen

--- src/Field.hpp
+++ src/Field.hpp
@@ -3,10 +3,18 @@
 
 #include <string>
 
+/**
+ * @brief Stores information of a Field of a bib-Element
+ */
 struct Field {
-    std::string name;
-    std::string value;
+    std::string name; ///<@property The name of the Field
+    std::string value; ///<@property The value of the Field
 
+    /**
+     * @brief Constructor.
+     * @param name Name of the Field
+     * @param value Value of the Field
+     */
     Field(std::string name, std::string value);
 };
 
