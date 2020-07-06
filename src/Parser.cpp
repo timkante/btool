afd4d4e5f477f86a6a9973d3b5164230aedc1abe
duneer <tom.seidel.schlema@gmail.com>
2020-07-06 14:30:53 +0200

    änderungen am Parser
    Co-Authored-By: kevindiex <kevindiex@users.noreply.github.com>
    Co-Authored-By: martinzeidler <martinzeidler@users.noreply.github.com>

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -18,28 +18,34 @@ std::vector<BibElement> Parser::parseFile(boost::filesystem::ifstream &fsStream)
     std::regex expr{"\\@(.*)\\{(.*)\\,"};
     std::smatch group;
     std::string fieldName;
-    bool styleMatch = false;
     std::optional<StyleProperties> targetStructure = translationTable.stylePropertiesOf(targetStyle);
+    int requiredFieldSize = targetStructure->requiredFields.size();
+    bool styleMatch = false;
     while (std::getline(fsStream, line)) {
         boost::trim(line);
         if (std::regex_search(line, group, expr)) {
             styleMatch = group[1] == targetStyle;
             if (bibElem.id != "" && styleMatch) {
+                if (requiredFieldSize > 0)
+                    throw std::invalid_argument("Not all required Fields set in Element with ID: " + bibElem.id);
                 bibElems.insert(bibElems.end(), bibElem);
             }
             if (styleMatch) {
                 bibElem = parseElement(group[1], group[2], StyleProperties());
+                requiredFieldSize = targetStructure->requiredFields.size();
             } else {
                 continue;
             }
 
         } else if (styleMatch) {
-            Field field = parseField(line, targetStructure);
+            Field field = parseField(line, targetStructure, (int &) requiredFieldSize);
             if (field.name != "") {
                 bibElem.attributes.insert(bibElem.attributes.end(), field);
             }
         }
     }
+    if (requiredFieldSize > 0)
+        throw std::invalid_argument("Not all required Fields set in Element with ID: " + bibElem.id);
     bibElems.insert(bibElems.end(), bibElem);
     return bibElems;
 }
@@ -64,16 +70,20 @@ void Parser::parseFiles(const boost::filesystem::path &path) {
     }
 }
 
-Field Parser::parseField(std::string field, std::optional<StyleProperties> targetStructure) {
+Field Parser::parseField(std::string field, std::optional<StyleProperties> targetStructure, int& requiredFieldSize) {
     std::regex fieldExpr{"^\\s*(.*)\\s+=\\s+\\{*(.*?)\\}*\\,?$"};
     std::smatch group;
     if (std::regex_search(field, group, fieldExpr)) {
 
         std::string fieldName = group[1];
         boost::trim(fieldName);
+
         if (std::find(targetStructure->requiredFields.begin(), targetStructure->requiredFields.end(), fieldName) !=
-            targetStructure->requiredFields.end()
-            || std::find(targetStructure->optionalFields.begin(), targetStructure->optionalFields.end(), fieldName) !=
+            targetStructure->requiredFields.end()){
+            requiredFieldSize--;
+            return Field(fieldName, replaceUmlaut(group[2]));
+        }
+            if(std::find(targetStructure->optionalFields.begin(), targetStructure->optionalFields.end(), fieldName) !=
                targetStructure->optionalFields.end()) {
             return Field(fieldName, replaceUmlaut(group[2]));
         }
@@ -95,24 +105,46 @@ BibElement Parser::parseElement(std::string style, std::string id, StyleProperti
 
 void Parser::checkFolder(const boost::filesystem::path &path) {
     if (!boost::filesystem::is_directory(path)) {
-        std::cout << "Kein Ordner!\n";
+        throw std::invalid_argument("No Folder!");
     }
 }
 
 
-std::string Parser::replaceUmlaut(std::string value) {
+std::string Parser::replaceUmlaut(std::string line) {
     std::regex expr{"(\\{.*?\\})"};
     std::smatch group;
+    std::string value = line;
     std::regex_token_iterator<std::string::iterator> rend;
-    std::regex_token_iterator<std::string::iterator> tokenIterator(value.begin(), value.end(), expr);
+    std::regex_token_iterator<std::string::iterator> tokenIterator(line.begin(), line.end(), expr);
     if (std::regex_search(value, group, expr)) {
-        //std::cout << group[1] << std::endl;
-        std::cout << value << std::endl;
-
         while (tokenIterator != rend) {
-            std::cout << *tokenIterator++ << "\t";
+            std::string toConvert = (*tokenIterator).str();
+            std::string umlaut = convertUmlaut(toConvert);
+            std::size_t pos = value.find(toConvert);
+
+            if (pos != std::string::npos) {
+                value.replace(pos, toConvert.length(), umlaut);
+            }
+
+            *tokenIterator++;
         }
-        std::cout << std::endl << std::endl;
     }
     return value;
+}
+
+std::string Parser::convertUmlaut(std::string toConvert) {
+    if (toConvert == "{\\\"a}")
+        return "ä";
+    if (toConvert == "{\\\"o}")
+        return "ö";
+    if (toConvert == "{\\\"u}")
+        return "ü";
+    if (toConvert == "{\\ss}")
+        return "ß";
+    if (toConvert == "{\\\'a}")
+        return "á";
+    if (toConvert == "{\\\'e}")
+        return "é";
+
+    throw std::invalid_argument("no valid Umlaut: " + toConvert);
 }
