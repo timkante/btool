9a2ceb455fb9c5f61cc73327823b1ac5e27955aa
Tim Kante <tim@timkante.dev>
2020-07-15 20:47:46 +0200

    :recycle: fixed compile-warnings

--- test/FieldTest.cpp
+++ test/FieldTest.cpp
@@ -4,6 +4,10 @@
 using namespace std::literals::string_literals;
 
 TEST(FieldTests, constructionTest) {
-  ASSERT_NO_THROW((Field{"a name"s, "a value"s}));
-  ASSERT_NO_THROW((Field{"another name"s, "another value"s}));
+  ASSERT_NO_THROW(([&](){
+    const auto test = Field{"a name"s, "a value"s};
+  }()));
+  ASSERT_NO_THROW(([&](){
+    const auto test = Field{"another name"s, "another value"s};
+  }()));
 }
