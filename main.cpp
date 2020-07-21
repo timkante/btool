b2776e473f0a0a1261d98dc4f7efd8fa326eb0c5
Tim Kante <tim@timkante.dev>
2020-07-21 13:30:29 +0200

    :fire: removed useless catch

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ struct [[maybe_unused]] Dependencies {
  * @param argv command-line arguments
  * @return program exit-code
  */
-int main(int argc, char **argv) try {
+int main(int argc, char **argv) {
   po::options_description desc("Allowed options");
   desc.add_options()
       ("help,h", "print usage message")
@@ -138,8 +138,5 @@ int main(int argc, char **argv) try {
     }
   }
   return 0;
-} catch (std::exception &e) {
-  std::cerr << e.what() << "\n";
-  return -1;
 }
 
